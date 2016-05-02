6e4d7c708892340649c81bffd49567712b8c2b8f
Nicholas Smith <nicholasparkersmith@gmail.com>
2016-05-02 10:18:25 -0400

    using full dir path

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,11 +1,11 @@
 module.exports = function (gulp, plugins, paths) {
 	return function () {
 		var stream = gulp.src([
-			paths.bootstrap.scripts,
-			paths.scripts
+			'./node_modules/bootstrap/dist/js/bootstrap.js',
+			__dirname + '/../src/js/**/*.js',
 		])
 		.pipe(plugins.concat('design-system.js'))
-		.pipe(gulp.dest('dist/js'));
+		.pipe(gulp.dest(__dirname + '/../dist/js'));
 		return stream;
 	};
 };
