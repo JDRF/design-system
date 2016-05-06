b0c67bb50f2745d6353bf03874044d4355ed88f2
Nicholas Smith <nicholasparkersmith@gmail.com>
2016-05-06 17:08:36 -0400

    adding in all the button html files

--- tasks/assemble.js
+++ tasks/assemble.js
@@ -1,15 +1,20 @@
 module.exports = function (gulp, plugins, config) {
 	return function (done) {
+
+		var start = '<pre class="language-markup"><code class="language-markup">';
+		var end = '</code></pre>';
+
 		var stream = plugins.assemble({
 			dest: 'dist',
 			logErrors: config.dev,
 			helpers: {
 				example: function(args) {
-					var string, esacped_string, template;
+					var string, esacped_string, template_string, template;
 
 					string = args.replace( '.', '/' );
 					escaped = plugins.escape( plugins.fs.readFileSync(__dirname + '/../src/html/' + string + '.html', 'utf8') );
-					template = plugins.hbs.compile( escaped );
+					template_string = start + escaped + end;
+					template = plugins.hbs.compile( template_string );
 					return new plugins.hbs.SafeString(template(args));
 				}
 			}
