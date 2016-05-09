6babc9915e31312e55c9ded4f13af67c67f18d72
Nicholas Smith <nicholasparkersmith@gmail.com>
2016-05-09 16:45:59 -0400

    adding in jscsrc package

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -5,6 +5,8 @@ module.exports = function (gulp, plugins, paths) {
 			//'./node_modules/bootstrap/dist/js/bootstrap.js',
 			__dirname + '/../src/js/**/*.js',
 		])
+		.pipe(plugins.jscs())
+		.pipe(plugins.jscs.reporter())
 		.pipe(plugins.concat('design-system.js'))
 		.pipe(gulp.dest(__dirname + '/../dist/js'));
 		return stream;
