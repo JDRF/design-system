7e978309c4cac66b71a4e5767d67a7390f4be22a
Rachel Vasquez <rachel.vasquez@10up.com>
2016-05-20 17:32:50 -0400

    added scss linter to fabricator styles, adjusted some parameters in JS lints, installed css-parse and css-stringily

--- tasks/styles-fabricator.js
+++ tasks/styles-fabricator.js
@@ -5,9 +5,13 @@ module.exports = function (gulp, plugins, config) {
 		* from gulpfile.js, rename it to f.css and place it in the dist folder path.
 		*/
 		var stream = gulp.src(config.src.styles.fabricator)
+			//SCSS LINT
+			.pipe(plugins.scsslint({
+				'config': './.scss-lint.yml'
+			}))
 			.pipe(plugins.sourcemaps.init())
 			.pipe(plugins.sass().on('error', plugins.sass.logError))
-			.pipe(plugins.prefix('last 1 version'))
+			.pipe(plugins.prefix('last 2 versions'))
 			.pipe(plugins.gulpif(!config.dev, plugins.csso()))
 			.pipe(plugins.rename('f.css'))
 			.pipe(plugins.sourcemaps.write())
