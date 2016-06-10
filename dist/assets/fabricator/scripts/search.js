2a2d16deef741dbf246a344ea3d8fe9d7bc65d28
Nicholas Smith <nicholasparkersmith@gmail.com>
2016-06-10 14:36:31 -0400

    compiling

--- dist/assets/fabricator/scripts/search.js
+++ dist/assets/fabricator/scripts/search.js
@@ -924,6 +924,7 @@ module.exports = {
 
 			if ( href === current ) {
 				helpers.addClass( item, 'ds-current' );
+				helpers.addClass( item.parentNode.querySelector( 'ul.ds-nav' ), 'ds-current' );
 			} else {
 				helpers.removeClass( item, 'ds-current' );
 			}
